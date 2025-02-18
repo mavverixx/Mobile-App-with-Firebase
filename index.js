66efcbba35e9c868bb9bd9cf40bcbb8c28a93549
Rikki Henry <rikkidhenry@gmail.com>
2025-02-18 12:10:04 +0000

    Add quotes to databaseURL in firebaseConfig
    Add quotes to databaseURL in firebaseConfig

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { getDatabase,
          remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 const firebaseConfig = {
-    databaseURL: https://leads-tracker-app-7bcad-default-rtdb.europe-west1.firebasedatabase.app/
+    databaseURL: "https://leads-tracker-app-7bcad-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(firebaseConfig)
